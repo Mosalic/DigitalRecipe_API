08737b2ec42546d75c5d7e29bc09bc4df9818879
Röttger <mona.roettger@gmail.com>
2018-11-15 02:13:40 +0100

    decide between json-Post (from WebApp) an none (Android app) in Login.php

--- login.php
+++ login.php
@@ -4,14 +4,18 @@
 	//insert file
 	require "dbconnection.php";
 	
-	$_POST = json_decode(file_get_contents("php://input"),true); //JSON-Object von React muss umgewandelt werden, muss für Android noch abgefangen werden (kein json)
+	//when json is postet $_POST is null and must decoded (from web app), android app posts not in json
+	if($_POST == null){
+		$_POST = json_decode(file_get_contents("php://input"),true); //JSON-Object von React muss umgewandelt werden, muss für Android noch abgefangen werden (kein json)
+	}
+	
 	
 	$user_id;
 	$user_name = $_POST["userName"]; //"userName" and "userPassword" declaration in Android Studios BackgroundWorker-Class
 	$user_password = $_POST["userPassword"];
 	$mysql_qry = "SELECT id FROM Users WHERE username COLLATE Latin1_General_CS LIKE '$user_name' AND password COLLATE Latin1_General_CS LIKE '$user_password';";  //id is a column in the database, Collate beachtet GroßundKleinschreibung, muss auch in Datenbank gesetzt werden
 	$result = mysqli_query($conLink, $mysql_qry);
-	//$datarow = mysqli_fetch_all($res); //alle Daten aus der Datenbank holen
+	
 	
 	//check if User is in database
 	if(mysqli_num_rows($result) > 0 ){
