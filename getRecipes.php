2c20a09dc899b81df253ddea44bf2be19b29a7a9
Röttger <mona.roettger@gmail.com>
2018-12-17 20:29:47 +0100

    customized the response of recipe data

--- getRecipes.php
+++ getRecipes.php
@@ -21,8 +21,9 @@
 		
 		//soll nur eine Klass für App und Web geben, hier muss differenziert werden, von wo die Anfrage kommt
 		if($user_role == "Patienten"){
-			$mysql_qry = "SELECT * FROM ( (Rezepte LEFT JOIN Patienten ON versichertennummer_fk = versichertennummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse )
-									WHERE versichertennummer_fk LIKE '$user_ID';";
+			$mysql_qry = "SELECT * FROM ( Rezepte LEFT JOIN Aerzte ON LANR_fk = LANR ) WHERE versichertennummer_fk LIKE '$user_ID';";
+			/*$mysql_qry = "SELECT * FROM ( (Rezepte LEFT JOIN Patienten ON versichertennummer_fk = versichertennummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse )
+									WHERE versichertennummer_fk LIKE '$user_ID';";*/
 			/*$mysql_qry = "SELECT * FROM Rezepte 
 									( (LEFT JOIN Patienten ON versichertennummer_fk = versichertennummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse )
 									( ( (LEFT JOIN Aerzte ON LANR_fk = LANR) LEFT JOIN Betriebsstaetten ON betriebs_nummer_fk = betriebs_nummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse) 
@@ -55,9 +56,10 @@
 			for($i=0;$i<count($datarow);$i++){
 				//echo '<br/>' . "Schleife " .$i .': ' ;
 				if($userRole == "Patienten"){
-					$data[$i] = [ 'id_recipe' => $datarow[$i][0], 'med_name' => $datarow[$i][1], 'med_form' => $datarow[$i][2], 'med_menge' => $datarow[$i][3], 'med_datum' => $datarow[$i][5], 'noctu' => $datarow[$i][6],
+					$data[$i] = [ 'id' => $datarow[$i][0], 'med_name' => $datarow[$i][1], 'med_form' => $datarow[$i][2], 'med_menge' => $datarow[$i][3], 'med_datum' => $datarow[$i][5], 'noctu' => $datarow[$i][6], 'aut_idem' => $datarow[$i][8], 'ver_nummer' => $datarow[$i][9], 'LANR_fk' => $datarow[$i][10], 'doc_lastName' => $datarow[$i][12], 'doc_firstName' => $datarow[$i][13], 'doc_title' => $datarow[$i][14] ];
+					/*$data[$i] = [ 'id_recipe' => $datarow[$i][0], 'med_name' => $datarow[$i][1], 'med_form' => $datarow[$i][2], 'med_menge' => $datarow[$i][3], 'med_datum' => $datarow[$i][5], 'noctu' => $datarow[$i][6],
 								'ver_nummer' => $datarow[$i][9], 'LANR_fk' => $datarow[$i][10], 'pat_lastName' => $datarow[$i][12], 'pat_firstName' => $datarow[$i][13], 'pat_geb' => $datarow[$i][14],
-								'pat_insurance' => $datarow[$i][15], 'adress_id' => $datarow[$i][18], 'adress_street' => $datarow[$i][19], 'adress_street_nr' => $datarow[$i][20], 'adress_PLZ' => $datarow[$i][22], 'adress_city' => $datarow[$i][23] ];
+								'pat_insurance' => $datarow[$i][15], 'adress_id' => $datarow[$i][18], 'adress_street' => $datarow[$i][19], 'adress_street_nr' => $datarow[$i][20], 'adress_PLZ' => $datarow[$i][22], 'adress_city' => $datarow[$i][23] ];*/
 				}else if($userRole == "Aerzte"){
 					$data[$i] = [ 'id' => $datarow[$i][0], 'med_name' => $datarow[$i][1], 'med_form' => $datarow[$i][2], 'med_menge' => $datarow[$i][3], 'ver_nummer' => $datarow[$i][9], 'LANR_fk' => $datarow[$i][10], 'pat_lastName' => $datarow[$i][12], 'pat_firstName' => $datarow[$i][13] ];
 				}
