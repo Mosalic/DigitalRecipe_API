821b53dd100b100adb7835b12641343112f9d7a1
Röttger <mona.roettger@gmail.com>
2018-12-22 17:33:07 +0100

    get all the data to show the  complete recipe

--- showRecipe.php
+++ showRecipe.php
@@ -23,19 +23,20 @@
 		//soll nur eine Klass für App und Web geben, hier muss differenziert werden, von wo die Anfrage kommt
 		if($user_role == "Patienten"){
 			
-			//1)
-			$mysql_qry = "SELECT * FROM ((Rezepte LEFT JOIN Patienten ON versichertennummer_fk = versichertennummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse)
-									WHERE id_rezept LIKE '$recipe_ID';";
+			//1)Test wegen Bug
+			/*$mysql_qry = "SELECT * FROM Rezepte LEFT JOIN Patienten ON versichertennummer_fk = versichertennummer LEFT JOIN Adressen ON id_adresse_fk = id_adresse
+									WHERE id_rezept LIKE '$recipe_ID';";*/
 									
-			/*//2)
-			$mysql_qry = "SELECT * FROM (((Rezepte LEFT JOIN Aerzte ON LANR_fk = LANR) LEFT JOIN Betriebsstaetten ON betriebs_nummer_fk = betriebs_nummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse)
+			//2) Test wegen Bug
+			/*$mysql_qry = "SELECT * FROM (((Rezepte LEFT JOIN Aerzte ON LANR_fk = LANR) LEFT JOIN Betriebsstaetten ON betriebs_nummer_fk = betriebs_nummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse)
 									WHERE id_rezept LIKE '$recipe_ID';";*/
 				
 			//$mysql_qry = "SELECT * FROM ( Rezepte LEFT JOIN Aerzte ON LANR_fk = LANR ) WHERE versichertennummer_fk LIKE '$user_ID';";
-			/*$mysql_qry = "SELECT * FROM Rezepte 
-									( (LEFT JOIN Patienten ON versichertennummer_fk = versichertennummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse )
-									( ( (LEFT JOIN Aerzte ON LANR_fk = LANR) LEFT JOIN Betriebsstaetten ON betriebs_nummer_fk = betriebs_nummer) LEFT JOIN Adressen ON id_adresse_fk = id_adresse) 
-									WHERE versichertennummer_fk LIKE '$user_ID';";*/
+			//3) Final, alle Daten passend zum Rezept
+			$mysql_qry = "SELECT * FROM Rezepte 
+									LEFT JOIN Patienten ON versichertennummer_fk = versichertennummer LEFT JOIN Adressen AS patAdresse ON Patienten.id_adresse_fk = patAdresse.id_adresse 
+									LEFT JOIN Aerzte ON LANR_fk = LANR LEFT JOIN Betriebsstaetten ON betriebs_nummer_fk = betriebs_nummer LEFT JOIN Adressen AS docAdresse ON Betriebsstaetten.id_adresse_fk = docAdresse.id_adresse
+									WHERE id_rezept LIKE '$recipe_ID';";
 									
 		}else if($user_role == "Aerzte"){
 			//noch nicht in Gebrauch
@@ -62,11 +63,18 @@
 			for($i=0;$i<count($datarow);$i++){
 				//echo '<br/>' . "Schleife " .$i .': ' ;
 				if($userRole == "Patienten"){
-					//1)
-					$data[$i] = [ 'ver_nummer' => $datarow[$i][11], 'id_adresse_fk' => $datarow[$i][16], 'id_adresse' => $datarow[$i][18] ];
+					//1)Test
+					//$data[$i] = [ 'ver_nummer' => $datarow[$i][11], 'id_adresse_fk' => $datarow[$i][16], 'id_adresse' => $datarow[$i][18] ];
 					
-					//2)
+					//2)Test
 					//$data[$i] = [ 'LANR' => $datarow[$i][11], 'betriebs_nummer' => $datarow[$i][18], 'id_adresse' => $datarow[$i][22] ];
+					
+					//3) Final
+					$data[$i] = [ 'ver_nummer' => $datarow[$i][11], 'pat_lastName' => $datarow[$i][12], 'pat_firstName' => $datarow[$i][13], 'pat_insurance' => $datarow[$i][15], 
+								'pat_adresse_street' => $datarow[$i][19], 'pat_adresse_street_nr' => $datarow[$i][20], 'pat_adresse_PLZ' => $datarow[$i][22], 'pat_adresse_city' => $datarow[$i][23],
+								'LANR' => $datarow[$i][24],'doc_lastName' => $datarow[$i][25], 'doc_firstName' => $datarow[$i][26], 'doc_title' => $datarow[$i][27], 'doc_signature' => $datarow[$i][28], 
+								'betriebs_nummer' => $datarow[$i][31], 'betriebs_name' => $datarow[$i][32], 'betriebs_phone' => $datarow[$i][33],
+								'doc_adresse_street' => $datarow[$i][36], 'doc_adresse_street_nr' => $datarow[$i][37], 'doc_adresse_PLZ' => $datarow[$i][39], 'doc_adresse_city' => $datarow[$i][40]];
 				}else if($userRole == "Aerzte"){
 					//$data[$i] = [ 'id' => $datarow[$i][0], 'med_name' => $datarow[$i][1], 'med_form' => $datarow[$i][2], 'med_menge' => $datarow[$i][3], 'ver_nummer' => $datarow[$i][9], 'LANR_fk' => $datarow[$i][10], 'pat_lastName' => $datarow[$i][12], 'pat_firstName' => $datarow[$i][13] ];
 				}
